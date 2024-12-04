2312beac3310faaf97a5508f1e72529d0f81c454
Dawid Gmerek <112812631+Zielonusek1@users.noreply.github.com>
2024-12-04 12:20:34 +0100

    as

--- z2_gmer_zewnetrzny.js
+++ z2_gmer_zewnetrzny.js
@@ -1,2 +1,2 @@
-strike(document.write("ostatnia modyfikacja strony".fontcolor("Green").bold().fontsize(6)+"<br>"));
+document.write(strike("ostatnia modyfikacja strony".fontcolor("Green").bold().fontsize(6)+"<br>"));
 document.write(document.lastModified .fontcolor("Green").bold().fontsize(6)+"<br");
